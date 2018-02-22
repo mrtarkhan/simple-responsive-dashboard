e617747805edde7c4f6790510f067838b1f05b5d
mrtarkhan <mrtarkhan@outlook.com>
2018-02-22 20:07:14 +0330

    [Fix]: menu logo z-index hide other elements, [Optimize]: LESS

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,10 +1,11 @@
 let ToggleSidebar = (event) => {
-	let elem = event.target;
+	let elem = document.getElementById('sidebar-toggle');
 	let children = [].slice.call(elem.children);
 	children.forEach(function (value) {
 		value.classList.toggle("toggle");
 	});
 	document.getElementById("sidebar").classList.toggle("toggle-sidebar");
+	document.getElementById("header-logo").classList.toggle("toggle-togglebar");
 	document.getElementById("main-container").classList.toggle("toggle-main-container");
 	
 };
